630363acb4094fbd75db3b3c3c3e0e1274d7a2e1
Ankit Kumar <akbasera202137@gmail.com>
2024-03-15 16:29:23 +0530

    set todo data to localstorage

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const todoLists = document.querySelector(".todoLists");
 const listValue = document.querySelector(".todoValue");
 
+// create addtodolist function
+const addTodoListLocalStorage = (todo)=>{
+    return localStorage.setItem('tododata',JSON.stringify(todo));
+};
+
 const addTodoList = (e)=>{
     e.preventDefault();
-    console.log("hello",e.target);
+    //console.log("hello",e.target);
+    addTodoListLocalStorage(listValue.value);
+    const liElement = document.createElement('li');
+    liElement.innerHTML = listValue.value;
+    todoLists.appendChild(liElement);
+
 }
 document.querySelector(".btn").addEventListener("click",(e)=>{
     addTodoList(e);
