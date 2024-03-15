57923c564b4d00a732e0945c1a18b0564c1b8d67
Ankit Kumar <akbasera202137@gmail.com>
2024-03-15 18:24:36 +0530

    show localstorage data to ui

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const todoLists = document.querySelector(".todoLists");
 const listValue = document.querySelector(".todoValue");
 
+// create a list for local storage data
+let todoListValue = [];
+
+// function to get the data from local storage
+const getTodoListFromLocalStorage = ()=>{
+    return JSON.parse(localStorage.getItem("tododata")) || [];
+};
 // create addtodolist function
 const addTodoListLocalStorage = (todo)=>{
     return localStorage.setItem('tododata',JSON.stringify(todo));
 };
-
+ 
+const showTodoList = ()=>{
+    todoListValue = getTodoListFromLocalStorage();
+    todoListValue.forEach((currTodo)=>{
+        const liElement = document.createElement('li');
+        liElement.innerHTML = currTodo;
+        todoLists.appendChild(liElement);
+    })
+}
 const addTodoList = (e)=>{
     e.preventDefault();
     //console.log("hello",e.target);
-    addTodoListLocalStorage(listValue.value);
-    const liElement = document.createElement('li');
-    liElement.innerHTML = listValue.value;
-    todoLists.appendChild(liElement);
+    todoListValue = getTodoListFromLocalStorage();
+    let newTodo = listValue.value.trim();
+    console.log(newTodo);
+    listValue.value = "";
 
-}
+    if(newTodo.length != 0){
+        console.log(typeof todoListValue)
+        todoListValue.push(newTodo);
+        addTodoListLocalStorage(todoListValue);
+        const liElement = document.createElement('li');
+        liElement.innerHTML = listValue.value;
+        todoLists.appendChild(liElement);
+    }
+
+};
+showTodoList();
 document.querySelector(".btn").addEventListener("click",(e)=>{
     addTodoList(e);
 })
